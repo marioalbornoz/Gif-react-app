9a0519902eda087c6b92f6ee2f7201226b2e0748
marioalbornoz <mario.albornozc@utem.cl>
2020-07-21 15:57:42 -0400

    Se agrega state de error

--- src/components/AgregarCategoria.jsx
+++ src/components/AgregarCategoria.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const AgregarCategoria = ({actualizarCategoria, categorias}) => {
     const [valor, agregarValor] = useState('Hola Mundo');
+    const [Error, actualzarError] = useState(false);
 
     // Toma el valor ingresado en el input y actuañiza el state
     const cambioInput = (e) => {
@@ -11,13 +12,20 @@ const AgregarCategoria = ({actualizarCategoria, categorias}) => {
     // Actualiza componente principal
     const eventoSubmit = (e) => {
       e.preventDefault();
-      actualizarCategoria([...categorias, valor]);
+      if(valor.trim()===''){
+        actualzarError(true);
+      } else {
+        actualizarCategoria([...categorias, valor]);
+        actualzarError(false);
+      }
+      
     };
     
     return (
       <form
         onSubmit = {eventoSubmit}
       >
+        { Error ? 'Error campo de entrada vacio' : null}  
         <input 
             type="text"
             value={valor}
