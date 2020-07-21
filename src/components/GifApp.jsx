438cd2332daf8ea2c0b3c3040363a05295817639
marioalbornoz <mario.albornozc@utem.cl>
2020-07-20 23:34:46 -0400

    añadido evento onclick para agregar categoria

--- src/components/GifApp.jsx
+++ src/components/GifApp.jsx
@@ -3,10 +3,20 @@ import React, { useState } from 'react';
 const GifApp = () => {
     // const categorias = ['One Puch', 'Hunter X', 'Dragon Ball']
     const [categorias, actualizarCategoria] = useState(['One Puch', 'Hunter X', 'Dragon Ball']);
+
+    const handleAdd = ()=>{
+        actualizarCategoria([...categorias,
+            'Steins Gate']
+            )
+    }
     return ( 
         <div>
             <h2>GifApp</h2>
             <hr/>
+            <button
+                onClick={handleAdd}
+                >Agregar
+            </button>
             <ol>
             {
                 categorias.map(categoria => {
